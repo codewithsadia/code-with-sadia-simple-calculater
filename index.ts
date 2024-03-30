3cecffc9e4ef7426a2e1b168752488ef8b7e11cc
Sadia Imran <164052169+codewithsadia@users.noreply.github.com>
2024-03-30 13:17:11 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+//Printing a wellcome message
+console.log("\n\twellcome To\"codewithsadia\"-cli simple calculator\n");
+
+//Asking Question from users through inquirer
+const answers = await inquirer.prompt([
+    { message: "Enter first Number", type: "number", name: "firstNumber" },
+    { message: "Enter second Number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+
+console.log(answers);
+
+//conditional statements if else
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("please select valid operator");
+}
+
+
